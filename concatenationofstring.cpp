458e91391fdef9f88344d6541fabd82830aaa332
Lak5hay <tlakshay98@gmail.com>
2024-10-24 02:13:03 +0530

    Create concatenationofstring.cpp

---
+++ concatenationofstring.cpp
@@ -0,0 +1,15 @@
+//lakshay 23070123079
+#include<iostream>
+using namespace std;
+int main()
+{
+    string first_name,last_name;
+    first_name = "lakshay";
+    last_name = "tanwar";
+
+    cout<<first_name+last_name;
+}
+
+/*
+lakshaytanwar
+*/
